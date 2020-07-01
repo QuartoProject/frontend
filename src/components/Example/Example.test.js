8aac641a878a350f977c73b4fc10589ea44c0b6d
Beto <betotoro0902@gmail.com>
2020-06-30 21:21:32 -0500

    <Fix>: import of home in app.js

--- src/components/Example/Example.test.js
+++ src/components/Example/Example.test.js
@@ -1,7 +1,7 @@
-// import React from 'react';
-// import { render } from '@testing-library/react';
+import React from 'react';
+import { render } from '@testing-library/react';
 
-// test('Hello world', () => {
-//   const { getByText } = render(<p>Hello Jest</p>);
-//   expect(getByText('Hello Jest')).toBeTruthy();
-// });
+test('Hello world', () => {
+  const { getByText } = render(<p>Hello Jest</p>);
+  expect(getByText('Hello Jest')).toBeTruthy();
+});
