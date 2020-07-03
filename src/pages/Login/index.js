2acf4f47a8ca57e996df36d1dcfe200a116c4b5a
Beto <betotoro0902@gmail.com>
2020-07-03 09:28:54 -0500

    <Feature>: page whitout navbar and footer

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -8,31 +8,29 @@ import Logo from '../../assets/images/quarto.svg';
 
 const index = () => {
   return (
-    <>
-      <main>
-        <Navbar />
-        <article className="login">
-          <section className="login__image">
-            <img src={Logo} alt="Brand logo" />
-          </section>
-          <section className="login__content">
-            <h1>Iniciar sesión</h1>
-            <form className="form">
-              <div className="log form__name">
-                <label htmlFor="name">Nombre</label>
-                <input className="input name" type="text" id="name" />
-              </div>
-              <div className="log form__password">
-                <label htmlFor="password">contraseña</label>
-                <input className="input" type="password" id="password" />
-              </div>
-              <input type="submit" value="Inicia sesión" />
-            </form>
-          </section>
-        </article>
-        <Footer />
-      </main>
-    </>
+    <main>
+      {/* <Navbar /> */}
+      <article className="login">
+        <section className="login__image">
+          <img src={Logo} alt="Brand logo" />
+        </section>
+        <section className="login__content">
+          <h1>Iniciar sesión</h1>
+          <form className="form">
+            <div className="log form__name">
+              <label htmlFor="name">Email</label>
+              <input className="input name" type="email" id="name" />
+            </div>
+            <div className="log form__password">
+              <label htmlFor="password">contraseña</label>
+              <input className="input" type="password" id="password" />
+            </div>
+            <input type="submit" value="Inicia sesión" />
+          </form>
+        </section>
+      </article>
+      {/* <Footer /> */}
+    </main>
   );
 };
 
