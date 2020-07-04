3c1f7c5333914bd5654f49d2a8a51c49d365aff8
Miguel Angel Martelo Quiroz <miguemartelo1031@gmail.com>
2020-07-03 22:20:37 -0500

    <Feature>: Add Not Found page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import NoMatch from './pages/NoMatch';
 
 const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-      </Switch>
-    </Router>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
+      <Route path="*" component={NoMatch} />
+    </Switch>
   );
 };
 
