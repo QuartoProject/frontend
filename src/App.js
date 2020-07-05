ec76f76bedfdcdfed862625fb7b3af292300ee06
Mario A. Crespo Reyes  @maoacr <maoacr@gmail.com>
2020-07-04 20:48:55 -0500

    <Feature>: Branch Favoritos ready for production

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import NoMatch from './pages/NoMatch';
+import Favoritos from './pages/Favoritos';
 
 const App = () => {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route path="/favoritos" component={Favoritos} />
       <Route path="/login" component={Login} />
       <Route path="/register" component={Register} />
       <Route path="*" component={NoMatch} />
