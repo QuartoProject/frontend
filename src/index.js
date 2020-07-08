aff0ee9d2019cedc62f6db2beddeb4c5f28fd6e7
Miguel Angel Martelo Quiroz <miguemartelo1031@gmail.com>
2020-07-07 19:11:20 -0500

    <Fix>: update basename in router

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
-  <Router basename={process.env.REACT_APP_ROUTER_BASE || ''}>
+  <Router basename="/frontend/">
     <App />
   </Router>,
   document.getElementById('app')
