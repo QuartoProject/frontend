23f50e31d0224ac63449bb895098fa01bc048251
Miguel Angel Martelo Quiroz <miguemartelo1031@gmail.com>
2020-07-09 23:00:51 -0500

    <Feature>: Register functionality with error handlers

--- src/components/RegisterForm/index.js
+++ src/components/RegisterForm/index.js
@@ -1,17 +1,29 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 const ENDPOINT = 'https://quarto-backend.herokuapp.com/api';
 import axios from 'axios';
 
 const Register = () => {
+  const [name, setName] = useState('');
+  const [lastname, setLastname] = useState('');
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  let history = useHistory();
 
   const handleChange = (e) => {
     const { name, value } = e.target;
 
     switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'lastname':
+        setLastname(value);
+        break;
       case 'username':
         setUsername(value);
         break;
@@ -28,18 +40,50 @@ const Register = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    console.log({ name, lastname, username, email, password });
     axios
       .post(`${ENDPOINT}/register/`, {
+        name,
+        lastname,
         username,
         email,
         password,
+        anfitrion: true,
       })
-      .then((res) => console.log(res))
-      .catch((err) => console.error(err.message));
+      .then((res) => {
+        setError(false);
+        history.push('/');
+      })
+      .catch((err) => {
+        setError(true);
+        setErrorMessage(err.response.data.email[0]);
+      });
   };
 
   return (
     <form className="form" onSubmit={handleSubmit}>
+      <div className="log">
+        <label htmlFor="name">Nombre</label>
+        <input
+          className="input name"
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div className="log">
+        <label htmlFor="lastname">Apellido</label>
+        <input
+          className="input name"
+          type="text"
+          name="lastname"
+          value={lastname}
+          onChange={handleChange}
+          required
+        />
+      </div>
       <div className="log">
         <label htmlFor="username">Username</label>
         <input
@@ -73,6 +117,7 @@ const Register = () => {
           required
         />
       </div>
+      <div className="handleError">{error && <h2>{errorMessage}</h2>}</div>
       <input type="submit" value="Regístrate" />
     </form>
   );
