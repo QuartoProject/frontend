c030e35b478faad204843f49b2adb06dce35bfad
Miguel Angel Martelo Quiroz <miguemartelo1031@gmail.com>
2020-07-10 22:34:40 -0500

    <Test>: Add testing to Favorites Page

---
+++ src/pages/Favorites/Favorites.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+
+import Favoritos from './index';
+
+describe('Favorites Test', () => {
+  test('Title of page', () => {
+    const { getByTestId } = render(
+      <MemoryRouter>
+        <Favoritos />
+      </MemoryRouter>
+    );
+
+    expect(getByTestId('title')).toHaveTextContent('Favoritos');
+  });
+});
