cb0b75a1ccb948ab699f379adf0df18235945418
Miguel Angel Martelo Quiroz <miguemartelo1031@gmail.com>
2020-07-11 12:10:16 -0500

    <Test>: Add testing to Footer component

--- src/components/ListOfRooms/index.js
+++ src/components/ListOfRooms/index.js
@@ -5,6 +5,7 @@ import Room from '../Room';
 
 const ListOfRooms = () => {
   const rooms = useContext(Context);
+  if (!rooms) return <p>Loading...</p>;
   return rooms.map((room) => <Room key={room.id} {...room} />);
 };
 
